5cec64d96c1e4a19fab4e317baa97917a0c81f15
HackerU <Hackeru@Teacher.local>
2016-06-06 11:34:51 +0300

    post

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'API Home Page' });
 });
  
- var pg = require('pg');
 
 router.get('/api/initdb', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -38,6 +37,23 @@ router.get('/api/getnotes', function (request, response) {
 router.get('/api/addnote', function (request, response) {
 	var title = request.query.title;
 	var message = request.query.message;
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  	//INSERT INTO notes(title, message) VALUES('tomer', 'moshe');
+    client.query('INSERT INTO notes(title, message) VALUES('+title+','+message+');', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.json(result );}
+    });
+  });
+})
+
+
+router.post('/api/addnote', function (request, response) {
+	var title = request.body.title;
+	var message = request.body.message;
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   	//INSERT INTO notes(title, message) VALUES('tomer', 'moshe');
